ac7296e12c3047125dec2aabb6d936828f865216
Atanassoff <50939943+Atanassoff@users.noreply.github.com>
2025-03-13 14:50:57 +0200

    Add files via upload

---
+++ JSF_5_Lab_1_MultiplyNumberBy2.js
@@ -0,0 +1,5 @@
+function multiplyNumberBy2(n){
+    console.log(n * 2);
+}
+
+multiplyNumberBy2(4)
---
+++ JSF_5_Lab_2_StudentInformation.js
@@ -0,0 +1,10 @@
+function studentInformation(name, age, grade){
+    console.log(`Name: ${name}, Age: ${age}, Grade: ${grade.toFixed(2)}`);  
+}
+
+studentInformation('John', 15, 5.54678)
+//Name: John, Age: 15, Grade: 5.55
+studentInformation('Steve', 16, 2.1426) 
+//Name: Steve, Age: 16, Grade: 2.14
+studentInformation('Marry', 12, 6.00)
+//Name: Marry, Age: 12, Grade: 6.00 
